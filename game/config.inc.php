c384ce7f3a406e595d02de46fa9400ae7abf4104
owlg <owlg@users.noreply.github.com>
2014-02-04 22:00:11 +0800

    Create config.inc.php

---
+++ game/config.inc.php
@@ -0,0 +1,10 @@
+<?php
+$cfg['sql']['host'] = "localhost";
+$cfg['sql']['user'] = "PEATL";
+$cfg['sql']['pass'] = "";
+$cfg['sql']['name'] = "etl";
+
+$con = @mysql_connect($cfg['sql']['host'], $cfg['sql']['user'], $cfg['sql']['pass']) or die("<h1>Unable to connect to mySQL Server</h1><br /><h3>Please try again in the near future.</h3><p>If you are seeing this message, it means the mySQL Server or the host is currently offline.<br />Please bear with us as we don't have enough funds to afford a VPS. We are proudly to accept your donations so we can afford a VPS and avoid these messages.<br /><br />~ WiSeXC Staff ~</p><br />");
+mysql_select_db($cfg['sql']['name'], $con) or die();
+error_reporting(0);
+?>
